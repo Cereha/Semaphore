618a6ccf1978cfdfb25402767d912bd2d52ce24a
Koroban Olya <79316653+Cereha@users.noreply.github.com>
2021-06-02 15:53:39 +0300

    Update Program.cs

--- Labo3/Program.cs
+++ Labo3/Program.cs
@@ -1,15 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
+using System.Threading;
 
 namespace Labo3
 {
     class Program
     {
+        public static Semaphore _semaphore = new Semaphore(3, 3, "Labo3");
         static void Main(string[] args)
         {
+            if (_semaphore.WaitOne(1000))
+            {
+                Console.WriteLine("IM Alive");
+                Console.WriteLine("Press any key");
+                Console.ReadKey();
+                _semaphore.Release();
+                _semaphore.Dispose();
+            }
+            else
+            {
+                Console.WriteLine("Cannot obtain semaphore");
+            }
         }
     }
 }
